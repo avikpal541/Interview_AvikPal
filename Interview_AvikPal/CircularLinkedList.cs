f7276303ddedbaed46f814ae7092653f8f931a28
avikpal541 <92321903+avikpal541@users.noreply.github.com>
2021-10-11 20:46:07 +0530

    Initial Checkin

---
+++ Interview_AvikPal/CircularLinkedList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview_AvikPal
+{
+    public class CircularLinkedList
+    {
+        public class Node
+        {
+            public string data;
+            public Node next;
+
+            public Node(string d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public Node head;
+
+        // Constructor
+        public CircularLinkedList()
+        {
+            head = null;
+        }
+
+        /* function to insert a new_node
+          in a list in sorted way. Note that
+          this function expects a pointer
+          to head node as this can modify
+          the head of the input linked list */
+        public void sortedInsert(Node new_node)
+        {
+            Node current = head;
+
+            // Case 1 : Linked List is empty
+            if (current == null)
+            {
+                new_node.next = new_node;
+                head = new_node;
+
+            }
+
+            // Case 2 : New node is to be inserted just before the head node
+            else if (current.data.CompareTo(new_node.data) <0)
+            {
+
+                /* If value is smaller than
+                    head's value then we need
+                    to change next of last node */
+                while (current.next != head)
+                    current = current.next;
+
+                current.next = new_node;
+                new_node.next = head;
+                head = new_node;
+            }
+
+            // Case 3 : New node is to be  inserted somewhere after the head
+            else
+            {
+
+                /* Locate the node before
+                the point of insertion */
+                while (current.next != head &&
+                    current.data.CompareTo(new_node.data) > 0)
+                    current = current.next;
+
+                new_node.next = current.next;
+                current.next = new_node;
+            }
+        }
+
+        // Utility method to print a linked list
+        public void printList()
+        {
+            if (head != null)
+            {
+                Node temp = head;
+                do
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                while (temp != head);
+            }
+        }
+    }
+}
---
+++ Interview_AvikPal/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using static Interview_AvikPal.CircularLinkedList;
+
+namespace Interview_AvikPal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GIT API connection
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.github.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method
+                HttpResponseMessage response = client.GetAsync("repositories/2126244/commits?per_page=100&sha=7a8d6b19767a92b1c4ea45d88d4eedc2b29bf1fa").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                }
+                else
+                {
+
+                }
+            }
+
+
+            String str = string.Empty;
+            Console.WriteLine("GIT API Connection issue. Please Enter a comment as input : ");
+            str=Console.ReadLine();
+            string[] strarr = str.Split(' ');
+
+            //Circular Linked List 
+            CircularLinkedList list = new CircularLinkedList();
+
+            //Circular Linked List  insertion
+            for (int i = 0; i < strarr.Length; i++)
+            {
+                Node temp = new Node(strarr[i]);
+                list.sortedInsert(temp);
+            }
+
+            //Circular Linked List  insertion
+            Console.WriteLine("\n\nPrinting Words present in comment in decending order are : \n");
+            list.printList();
+
+            //Calling Bubble sort method
+            sortStrings(strarr);
+            Console.WriteLine("\n\nPrinting Words present in comment in accesding order are : ");
+            foreach (var item in strarr)
+            {
+                Console.WriteLine(item);
+            }
+
+            //CSV Export function call
+            string filename=CsvExport(CountWord(strarr));
+            Console.WriteLine("\n\nCsv File "+filename+" exported to Folder : ~\\bin\\Debug\\netcoreapp3.1 with count for each words in comment.");
+        }
+
+
+
+        public static void sortStrings(string[] arr)
+        {
+            string temp;
+
+            // Sorting strings using bubble sort
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                for (int i = j + 1; i < arr.Length; i++)
+                {
+                    if (arr[j].CompareTo(arr[i]) > 0)
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+
+
+        public static Dictionary<string, int> CountWord(string[] strarr)
+        {
+            int count = 1;
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            for (int i = 0, j = i + 1; i < strarr.Length; i++, j++)
+            {
+                if (j < strarr.Length)
+                {
+                    if (strarr[i].Equals(strarr[j]))
+                    {
+                        if (i == strarr.Length - 2)
+                        {
+                            count++;
+                            wordCount.Add(strarr[i], count);
+                        }
+                        count++;
+                        continue;
+                    }
+                    else
+                    {
+                        wordCount.Add(strarr[i], count);
+                        count = 1;
+                    }
+                }
+                else
+                {
+                    if (!wordCount.ContainsKey(strarr[i]))
+                        wordCount.Add(strarr[i], count);
+                }
+            }
+            Console.WriteLine("\n\nPrinting each Word count : ");
+            foreach(var item in wordCount)
+            {
+                Console.WriteLine(item.Key+" : "+item.Value);
+            }
+            return (wordCount);
+        }
+
+        public static string CsvExport(Dictionary<string, int> wordCount)
+        {
+            var file = @"WordCountFile_"
+                            + DateTime.Now.Year + "_"
+                            + DateTime.Now.Month + "_"
+                            + DateTime.Now.Day + "_"
+                            + DateTime.Now.Hour + "_"
+                            + DateTime.Now.Minute + "_"
+                            + DateTime.Now.Second + "_"
+                            + DateTime.Now.Millisecond
+                            + ".csv";
+            using (var stream = File.AppendText(file))
+            {
+                string csvHeader = string.Format("{0},{1}", "Word", "Count");
+                stream.WriteLine(csvHeader);
+
+                foreach (var item in wordCount)
+                {
+                    string csvRow = string.Format("{0},{1}", item.Key, item.Value);
+                    stream.WriteLine(csvRow);
+                }
+
+            }
+            return (file);
+        }
+    }
+}
